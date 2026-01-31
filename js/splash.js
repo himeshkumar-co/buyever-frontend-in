47311b57bdc0226abefeef6bd4171dbc03942c55
himeshkumar-co <himeshkumar.co@gmail.com>
2026-02-01 00:09:49 +0530

    Updated website and added logo

---
+++ js/splash.js
@@ -0,0 +1,6 @@
+window.onload = function(){
+  setTimeout(()=>{
+    document.getElementById("splash").style.display = "none";
+    document.getElementById("mainContent").style.display = "block";
+  }, 2500); // 2.5 seconds
+};
